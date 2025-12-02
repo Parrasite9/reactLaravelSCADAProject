6051bd02d55a135764bd71038e36309df99ca1c7
isaiah <isaiah.johnson@bigdco.com>
2025-12-02 16:22:28 -0600

    in case i fuck up

---
+++ resources/js/components/MyComponents/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar({page}) {
+    return (
+        <a href="{{ $href }}" class="{{$active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+            aria-current="{{ $active ? 'page' : 'false'}}"
+        >
+            {page}
+        </a>
+    )
+}
